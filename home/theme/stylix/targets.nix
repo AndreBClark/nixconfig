4235d78ba61e5a67a36bcc305a9605830b094aba
AndreBClark <andre@cosmicdivision.dev>
2025-12-22 20:50:06 -0800

    gtk: allow stylix to configure gtk and gnome stuff

--- home/theme/stylix/targets.nix
+++ home/theme/stylix/targets.nix
@@ -6,11 +6,7 @@
 {
   stylix = {
     targets = {
-      gtk.enable = false;
-      # gnome.enable = false;
-      # rofi.enable = false;
       firefox.profileNames = lib.mkIf config.programs.firefox.enable [ "dev-edition-default" ];
-      # gnome-text-editor.enable = lib.mkForce false;
       nixvim.plugin = "base16-nvim";
     };
   };
