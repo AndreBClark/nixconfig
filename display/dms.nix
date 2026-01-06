3233933ae86fc3a62ff226317e38b96ddb1d1eb9
AndreBClark <andre@cosmicdivision.dev>
2026-01-06 14:37:16 -0800

    greeter: setup dms-greeter (DONT USE)

--- display/dms.nix
+++ display/dms.nix
@@ -1,8 +1,24 @@
-{ ... }:
 {
-  # services.displayManager.sddm.enable = false;
-  # programs.dankMaterialShell.greeter = {
-  #   enable = true;
-  #   compositor.name = "niri";
-  # };
+  inputs,
+  config,
+  pkgs,
+  ...
+}:
+let
+  inherit (import ../variables/default.nix) username;
+in
+{
+  services.displayManager = {
+    sddm.enable = false;
+    dms-greeter = {
+      enable = true;
+      package = inputs.dms.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      compositor.name = "niri";
+      configFiles = [
+        "/home/${username}}/.config/DankMaterialShell/settings.json"
+        # "/home/${username}}/.local/state/DankMaterialShell/session.json"
+        # "/home/${username}/.cache/DankMaterialShell/dms-colors.json"
+      ];
+    };
+  };
 }
