16a10d272a5dea17abcb96edabfce4b8524b0083
AndreBClark <andre@cosmicdivision.dev>
2026-01-06 14:29:03 -0800

    niri: rename bindings.nix to binds.nix

--- home/workspace/niri/binds.nix
+++ home/workspace/niri/binds.nix
@@ -3,6 +3,7 @@
   binds {
     Mod+X hotkey-overlay-title="Power Menu" { spawn "dms" "ipc" "powermenu" "toggle"; }
     // programs
+    Mod+T hotkey-overlay-title="Open Terminal" { spawn "kitty"; }
     Mod+A { spawn-sh "vicinae toggle"; }
   }
 ''
