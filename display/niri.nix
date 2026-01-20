cdeb5112ba3840bc929a2d1747247ff5de0d6cb9
AndreBClark <andre@cosmicdivision.dev>
2026-01-19 21:23:50 -0800

    niri: use workaround to bring back default app settings for kde dolphin file manager

--- display/niri.nix
+++ display/niri.nix
@@ -7,14 +7,18 @@
     enable = true;
   };
 
-  environment.variables.NIXOS_OZONE_WL = "1";
-  environment.systemPackages = with pkgs; [
-    wl-clipboard
-    wayland-utils
-    libsecret
-    cage
-    gamescope
-    xwayland-satellite
-    xdg-desktop-portal-gnome
-  ];
+  environment = {
+    variables.NIXOS_OZONE_WL = "1";
+    systemPackages = with pkgs; [
+      wl-clipboard
+      wayland-utils
+      libsecret
+      cage
+      gamescope
+      xwayland-satellite
+      xdg-desktop-portal-gnome
+    ];
+    etc."xdg/menus/applications.menu".source =
+      "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+  };
 }
