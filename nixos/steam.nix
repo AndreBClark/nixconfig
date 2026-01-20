9882ea9306c9ac988c01de0a294c4aeddd336975
AndreBClark <andre@cosmicdivision.dev>
2026-01-19 17:10:12 -0800

    steam: add sgdboop package as it no longer fails to build from nixpkgs

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -1,42 +1,51 @@
 {
   pkgs,
-  username,
+  lib,
   ...
 }:
 {
-  environment.systemPackages = [
-    pkgs.xwayland
-    pkgs.samrewritten
-    pkgs.sgdboop
+  environment = {
+    systemPackages = [
+      pkgs.xwayland
+      pkgs.samrewritten
+      pkgs.sgdboop
+      pkgs.xsettingsd
+      pkgs.xorg.xrdb
+      pkgs.steamtinkerlaunch
+    ];
+  };
+  xdg.icons.fallbackCursorThemes = lib.mkForce [
+    "volantes_cursors"
+    "breeze_cursors"
   ];
-  programs.xwayland.enable = true;
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-    extraPackages = [
-      pkgs.gamescope
-      pkgs.gamemode
-      pkgs.openssl
-      pkgs.nghttp2
-      pkgs.libidn2
-      pkgs.rtmpdump
-      pkgs.libpsl
-      pkgs.curl
-      pkgs.krb5
-      pkgs.keyutils
-      pkgs.lsfg-vk
-      pkgs.lsfg-vk-ui
-      pkgs.volantes-cursors
-    ];
-    extraCompatPackages = [
-      pkgs.proton-ge-bin
-    ];
-    gamescopeSession.enable = true;
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      extraPackages = [
+        pkgs.gamescope
+        pkgs.gamemode
+        pkgs.openssl
+        pkgs.nghttp2
+        pkgs.libidn2
+        pkgs.rtmpdump
+        pkgs.libpsl
+        pkgs.curl
+        pkgs.krb5
+        pkgs.keyutils
+        pkgs.lsfg-vk
+        pkgs.lsfg-vk-ui
+        pkgs.volantes-cursors
+      ];
+      extraCompatPackages = [
+        pkgs.proton-ge-bin
+      ];
+      gamescopeSession.enable = true;
+    };
+    gamescope.enable = true;
+    xwayland.enable = true;
+    gamemode.enable = true;
   };
-  programs.gamescope = {
-    enable = true;
-  };
-  programs.gamemode.enable = true;
-  services.getty.autologinUser = username;
+  # services.getty.autologinUser = username;
 }
