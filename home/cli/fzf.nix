a0b9aaca30dab203792269fc0d3a1c6bdf7689b3
AndreBClark <andre@cosmicdivision.dev>
2026-02-05 20:27:56 -0800

    fix(cli/fzf): fix image rendering in file preview

--- home/cli/fzf.nix
+++ home/cli/fzf.nix
@@ -1,19 +1,60 @@
-{ lib, pkgs, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 let
   excludesList = [
+    "/home/${config.home.username}/.*"
     ".git"
     ".direnv"
     ".cache"
-    ".direnv/**"
+    ".pki"
+    ".dotnet"
     "pfx"
     "dosdevices"
     "*.lock"
+    # Dependencies and build outputs
+    "node_modules"
+    "target"
+    "build"
+    "dist"
+    "out"
+    "result"
+    # IDE and editor files
+    ".vscode"
+    ".idea"
+    "*.swp"
+    "*.swo"
+    "*~"
+    # OS-specific
+    ".DS_Store"
+    "Thumbs.db"
+    # Cache and temporary
+    ".cache"
+    "tmp"
+    "temp"
+    "*.tmp"
   ];
   excludeArgs = patterns: map (p: "--exclude ${lib.escapeShellArg p}") patterns;
   excludesStr = lib.concatStringsSep " " (excludeArgs excludesList);
   fdFlags = "--hidden --color=always --strip-cwd-prefix " + excludesStr;
   fdCommand = "fd --type f " + fdFlags;
   fdDirCommand = "fd --type d " + fdFlags;
+  customPreviewScript = pkgs.writeShellScript "kitty-preview.sh" ''
+    file=$1
+    type=$(file --brief --dereference --mime -- "$file")
+
+    if [[ $type =~ image/ ]]; then
+      dim=''${FZF_PREVIEW_COLUMNS}x''${FZF_PREVIEW_LINES}
+      kitten icat --clear --transfer-mode=stream --unicode-placeholder --stdin=no --place="$dim@0x0" "$file"
+    elif [[ $type =~ =binary ]]; then
+      file "$file"
+    else
+      env BAT_THEME=base16 ${pkgs.bat}/bin/bat --color=always "$file"
+    fi
+  '';
 in
 {
   programs = {
@@ -21,6 +62,11 @@ in
       enable = true;
       defaultCommand = fdCommand;
       fileWidgetCommand = fdCommand;
+      fileWidgetOptions = [
+        "--color=border:blue"
+        "--color=prompt:green"
+        "--color=info:bright-black"
+      ];
       changeDirWidgetCommand = "zoxide query -ls | ${fdDirCommand}";
       changeDirWidgetOptions = [
         "--preview='${pkgs.eza}/bin/eza --tree --color=always {}'"
@@ -37,7 +83,8 @@ in
         "--color=border:blue"
         "--color=prompt:green"
         "--color=info:bright-black"
-        "--preview='env BAT_THEME=base16 ${pkgs.fzf-preview}/bin/fzf-preview {}'"
+        "--preview '${customPreviewScript} {}'"
+        # "--preview='env BAT_THEME=base16 ${pkgs.fzf-preview}/bin/fzf-preview {}'"
         "--preview-window=60%,border-rounded"
       ];
     };
@@ -50,12 +97,15 @@ in
     };
   };
   home = {
-    packages = [ pkgs.fzf-preview ];
+    packages = [
+      pkgs.fzf-preview
+      pkgs.file
+    ];
     sessionVariables = {
       FZF_DEFAULT_COMMAND = fdCommand;
       FZF_CTRL_T_COMMAND = fdCommand;
       FZF_PREVIEW_COLUMNS = "80";
-      FZF_PREVIEW_LINES = "24";
+      FZF_PREVIEW_LINES = "80";
       _ZO_FZF_OPTS = fdFlags;
     };
   };
--- home/terminal/fish.nix
+++ home/terminal/fish.nix
@@ -3,6 +3,55 @@
   programs = {
     fish = {
       enable = true;
+      plugins = [
+        {
+          inherit (pkgs.fishPlugins.autopair) src;
+          name = "autopair";
+        }
+        {
+          inherit (pkgs.fishPlugins.done) src;
+          name = "done";
+        }
+        {
+          inherit (pkgs.fishPlugins.sponge) src;
+          name = "sponge";
+        }
+        {
+          inherit (pkgs.fishPlugins.grc) src;
+          name = "grc";
+        }
+        {
+          inherit (pkgs.fishPlugins.fifc) src;
+          name = "fifc";
+        }
+        {
+          name = "getopts";
+          src = pkgs.fetchFromGitHub {
+            owner = "jorgebucaran";
+            repo = "getopts.fish";
+            rev = "4b74206725c3e11d739675dc2bb84c77d893e901";
+            sha256 = "1z5jvqip1hx59cggj9fyzaqqpz5rrsdjb3kv6ha042pbd034a57n";
+          };
+        }
+        {
+          name = "colored_man_pages";
+          src = pkgs.fetchFromGitHub {
+            owner = "PatrickF1";
+            repo = "colored_man_pages.fish";
+            rev = "f885c2507128b70d6c41b043070a8f399988bc7a";
+            sha256 = "0ifqdbaw09hd1ai0ykhxl8735fcsm0x2fwfzsk7my2z52ds60bwa";
+          };
+        }
+        {
+          name = "fish-abbreviation-tips";
+          src = pkgs.fetchFromGitHub {
+            owner = "Gazorby";
+            repo = "fish-abbreviation-tips";
+            rev = "8ed76a62bb044ba4ad8e3e6832640178880df485";
+            sha256 = "05b5qp7yly7mwsqykjlb79gl24bs6mbqzaj5b3xfn3v2b7apqnqp";
+          };
+        }
+      ];
       interactiveShellInit = ''
         set --export fish_greeting
 
@@ -14,9 +63,9 @@
         la = "eza -A";
         lt = "eza -T";
         cd = "z";
+        vim = "nvim";
       };
     };
-
     man.generateCaches = false;
     kitty.shellIntegration.enableFishIntegration = true;
     starship.enableFishIntegration = true;
@@ -28,4 +77,5 @@
     direnv.enableFishIntegration = true;
     tmux.shell = "${pkgs.fish}/bin/fish";
   };
+  home.packages = [ pkgs.grc ];
 }
