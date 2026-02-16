b5f7f7c9ca3a8add65988ce00c25465ae8b18d4a
AndreBClark <andre@cosmicdivision.dev>
2026-02-16 01:32:02 -0800

    feat: add portal-gtk

--- nixos/xdg/portal.nix
+++ nixos/xdg/portal.nix
@@ -5,12 +5,14 @@
     xdgOpenUsePortal = true;
     wlr.enable = true;
     extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
       kdePackages.xdg-desktop-portal-kde
       xdg-desktop-portal-gnome
     ];
     configPackages = [
       pkgs.kdePackages.xdg-desktop-portal-kde
       pkgs.xdg-desktop-portal-gnome
+      pkgs.xdg-desktop-portal-gtk
     ];
   };
 }
