ec825fcde0bbe5eb4d394ee23293672c46758117
AndreBClark <andre@cosmicdivision.dev>
2026-02-16 22:19:29 -0800

    vscode

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -1,43 +1,38 @@
+{ pkgs, lib, ... }:
+let
+  theme = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "dracula-purple";
+      publisher = "cbasdev";
+      version = "1.5.2";
+      sha256 = "sha256-l14xKOxkh4w0q+dEwGhT48COB/xdTYrm/R5GOHrLgDg=";
+    };
+  };
+  vsc-ext = pkgs.vscode-extensions;
+in
 {
-  pkgs,
-  lib,
-  ...
-}: let
-    theme = (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-      mktplcRef = {
-        name = "dracula-purple";
-        publisher = "cbasdev";
-        version = "1.5.2";
-        sha256 = "sha256-l14xKOxkh4w0q+dEwGhT48COB/xdTYrm/R5GOHrLgDg=";
-      };
-    });
-in {
   programs.vscode = {
     enable = true;
     mutableExtensionsDir = true;
     profiles.default = {
       enableUpdateCheck = false;
       enableExtensionUpdateCheck = false;
-      userSettings = lib.importJSON(./settings.json);
-      extensions = with pkgs.vscode-extensions; [
-        svelte.svelte-vscode
-        vue.volar
-#         ms-dotnettools.csharp
-#         ms-dotnettools.vscode-dotnet-runtime
-#         ms-dotnettools.vscodeintellicode-csharp
-#         ms-dotnettools.csdevkit
-        woberg.godot-dotnet-tools
-        vscodevim.vim
-        yoavbls.pretty-ts-errors
-        vscode-icons-team.vscode-icons
-        visualstudioexptteam.vscodeintellicode
-        vincaslt.highlight-matching-tag
-        visualstudiotoolsforunity.vstuc
-        esbenp.prettier-vscode
-        dbaeumer.vscode-eslint
-        astro-build.astro-vscode
-        bradlc.vscode-tailwindcss
-        shardulm94.trailing-spaces
+      userSettings = lib.importJSON (./settings.json);
+      extensions = [
+        vsc-ext.svelte.svelte-vscode
+        vsc-ext.vue.volar
+        vsc-ext.woberg.godot-dotnet-tools
+        vsc-ext.vscodevim.vim
+        vsc-ext.yoavbls.pretty-ts-errors
+        vsc-ext.vscode-icons-team.vscode-icons
+        vsc-ext.visualstudioexptteam.vscodeintellicode
+        vsc-ext.vincaslt.highlight-matching-tag
+        vsc-ext.visualstudiotoolsforunity.vstuc
+        vsc-ext.esbenp.prettier-vscode
+        vsc-ext.dbaeumer.vscode-eslint
+        vsc-ext.astro-build.astro-vscode
+        vsc-ext.bradlc.vscode-tailwindcss
+        vsc-ext.shardulm94.trailing-spaces
         theme
       ];
     };
