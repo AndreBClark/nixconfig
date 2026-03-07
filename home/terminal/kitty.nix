4b99fe26965ad22707c121c3216384bcc9763d12
AndreBClark <andre@cosmicdivision.dev>
2026-03-06 17:14:57 -0800

    refactor(terminal/kitty): colorful tabs

--- home/terminal/kitty.nix
+++ home/terminal/kitty.nix
@@ -1,14 +1,27 @@
-{ pkgs, ... }:
+{ config, ... }:
+let
+  colors = config.lib.stylix.colors.withHashtag;
+in
 {
   programs = {
     kitty = {
       enable = true;
-      shellIntegration.enableFishIntegration = true;
       settings = {
-        # symbol_map = "U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b7,U+e700-U+e8ef,U+ed00-U+efc1,U+f000-U+f2ff,U+f000-U+f2e0,U+f300-U+f381,U+f400-U+f533,U+f0001-U+f1af0 Symbols Nerd Font Mono";
+        tab_title_template = "[{index}] {tab.active_wd}{activity_symbol}";
+        tab_title_max_length = 0;
         tab_bar_edge = "top";
+        tab_bar_style = "powerline";
+        tab_powerline_style = "round";
       };
+      extraConfig = ''
+        active_tab_background ${colors.base0C}
+        active_tab_foreground ${colors.base01}
+
+        inactive_tab_background ${colors.base0D}
+        inactive_tab_foreground ${colors.base02}
+      '';
     };
     tmux.terminal = "kitty";
   };
+
 }
