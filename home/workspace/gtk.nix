1ca645e12ee364805e21b5909270ac9c9fcfd8bf
AndreBClark <andre@cosmicdivision.dev>
2026-03-09 13:26:38 -0700

    fix(gtk): force overwrite of gtk css to mitigate hm clobbering error

--- home/workspace/gtk.nix
+++ home/workspace/gtk.nix
@@ -4,4 +4,8 @@
     gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
     gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
   };
+  xdg.configFile = {
+    "gtk-3.0/gtk.css".force = true;
+    "gtk-4.0/gtk.css".force = true;
+  };
 }
