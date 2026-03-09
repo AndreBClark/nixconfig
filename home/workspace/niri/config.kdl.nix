4a54a704abd86c01f546a4c39ee9c10606b58050
AndreBClark <andre@cosmicdivision.dev>
2026-03-09 13:26:05 -0700

    refactor(niri): set scale for laptop screen and move layout to file

--- home/workspace/niri/config.kdl.nix
+++ home/workspace/niri/config.kdl.nix
@@ -3,38 +3,44 @@
   pkgs,
   ...
 }:
-let
-  gaps = "16";
-  inset = "-${toString (lib.toIntBase10 gaps - 2)}";
-in
 {
   home.packages = [
     pkgs.volantes-cursors
   ];
   xdg.configFile = {
-    "niri/config.kdl" = {
-      text = ''
-        include "dms-includes.kdl"
-        include "window-rules.kdl"
-        include "binds.kdl"
-        prefer-no-csd
-        output "DP-2" {
-          focus-at-startup
-          variable-refresh-rate on-demand=true
-        }
-        output "HDMI-A-1" {
-          position x=0 y=720
-          variable-refresh-rate on-demand=true
-        }
+    "niri/config.kdl".text = /* kdl */ ''
+      include "dms-includes.kdl"
+      include "window-rules.kdl"
+      include "binds.kdl"
+      include "layout.kdl"
+      include "laptop-output.kdl"
 
-        workspace "1" {
-          open-on-output "DP-2"
-        }
-        workspace "2" {
-          open-on-output "HDMI-A-1"
-        }
-        workspace "3" { }
+      prefer-no-csd
 
+      output "DP-2" {
+        focus-at-startup
+        variable-refresh-rate on-demand=true
+      }
+      output "HDMI-A-1" {
+        position x=0 y=720
+        variable-refresh-rate on-demand=true
+      }
+
+      workspace "1" {
+        open-on-output "DP-2"
+      }
+      workspace "2" {
+        open-on-output "HDMI-A-1"
+      }
+      workspace "3" { }
+    '';
+
+    "niri/layout.kdl".text =
+      let
+        gaps = "16";
+        inset = "-${toString (lib.toIntBase10 gaps - 2)}";
+      in
+      /* kdl */ ''
         layout {
           gaps ${gaps}
           struts {
@@ -48,6 +54,5 @@ in
           }
         }
       '';
-    };
   };
 }
--- home/workspace/niri/laptop-output.kdl.nix
+++ home/workspace/niri/laptop-output.kdl.nix
@@ -2,6 +2,7 @@
   xdg.configFile."niri/laptop-output.kdl".text = /* kdl */ ''
     output "eDP-1" {
       mode custom=true "1920x1080@60"
+      scale 1.0
     }
   '';
 }
