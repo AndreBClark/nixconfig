cc6fec285b58936b901bd1cad1e51408125c5cc6
AndreBClark <andre@cosmicdivision.dev>
2026-04-10 19:46:15 -0700

    fix: cursor graphical artifacts no longer appear during greeter

--- nixos/greeter.nix
+++ nixos/greeter.nix
@@ -1,13 +1,30 @@
 { config, ... }:
+let
+  configHome = "/home/${config.username}";
+in
 {
   programs.niri.enable = true;
   services.displayManager = {
     sddm.enable = false;
     dms-greeter = {
+      inherit configHome;
       enable = true;
-      compositor.name = "niri";
-      logs.save = true;
-      configHome = "/home/${config.username}";
+      compositor = {
+        name = "niri";
+        customConfig = ''
+          hotkey-overlay {
+            skip-at-startup
+          }
+          cursor {
+            xcursor-theme "volantes_cursors"
+            xcursor-size 48
+          }
+          debug {
+            disable-cursor-plane
+            keep-max-bpc-unchanged
+          }
+        '';
+      };
     };
   };
 }
