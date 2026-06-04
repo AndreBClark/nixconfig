462a665c7c684465217c7e6379679600edaf2314
AndreBClark <andre@cosmicdivision.dev>
2026-06-03 21:41:31 -0700

    chore: update sources

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    millennium.url = "github:SteamClientHomebrew/Millennium?dir=packages/nix";
+    millennium.url = "github:SteamClientHomebrew/Millennium/e2c66a276e579ee73c5151b01897bf63503aa12c?dir=packages/nix";
   };
 
   outputs =
