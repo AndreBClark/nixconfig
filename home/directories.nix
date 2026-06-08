de9f861702e1288d5857fe869f0078b8925a6144
AndreBClark <andre@cosmicdivision.dev>
2026-06-07 22:43:48 -0700

    fix: set icon for folders to folder instead of inode-directory

--- home/directories.nix
+++ home/directories.nix
@@ -29,6 +29,14 @@ let
   };
 in
 {
+  xdg.dataFile."mime/packages/inode-directory.xml".text = ''
+    <?xml version="1.0" encoding="utf-8"?>
+    <mime-type xmlns="http://www.freedesktop.org/standards/shared-mime-info" type="inode/directory">
+      <comment>Folder</comment>
+      <icon name="folder"/>
+      <glob-deleteall/>
+    </mime-type>
+  '';
   home.activation.createDirectories = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     ${lib.concatMapStringsSep "\n" (dir: ''
       run mkdir -p ${homeDir}/${dir.path}
