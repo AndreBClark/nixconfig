4ff7d42d0b67c952d14aceb01f8cffad4ec3ad6f
AndreBClark <andre@cosmicdivision.dev>
2026-06-07 23:10:42 -0700

    fix: add mime inode override for folder icons

--- home/directories.nix
+++ home/directories.nix
@@ -31,11 +31,13 @@ in
 {
   xdg.dataFile."mime/packages/inode-directory.xml".text = ''
     <?xml version="1.0" encoding="utf-8"?>
-    <mime-type xmlns="http://www.freedesktop.org/standards/shared-mime-info" type="inode/directory">
-      <comment>Folder</comment>
-      <icon name="folder"/>
-      <glob-deleteall/>
-    </mime-type>
+    <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+      <mime-type type="inode/directory">
+        <comment>Folder</comment>
+        <icon name="folder"/>
+        <glob-deleteall/>
+      </mime-type>
+    </mime-info>
   '';
   home.activation.createDirectories = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     ${lib.concatMapStringsSep "\n" (dir: ''
