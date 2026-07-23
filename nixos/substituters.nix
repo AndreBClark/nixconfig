0a5cc25f36bd726963c486d868035b312d21bca5
AndreBClark <andre@cosmicdivision.dev>
2026-07-22 18:33:12 -0700

    build(substituters): add nix-community cachix cache

--- nixos/substituters.nix
+++ nixos/substituters.nix
@@ -3,10 +3,12 @@
     extra-substituters = [
       "https://niri.cachix.org"
       "https://devenv.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 }
