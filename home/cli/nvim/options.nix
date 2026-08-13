6407b69fcd7d402e95b5403c5ae1fa266a072f2a
AndreBClark <andre@cosmicdivision.dev>
2026-08-13 16:20:25 -0700

    style(nvim): remove redundant comments

--- home/cli/nvim/options.nix
+++ home/cli/nvim/options.nix
@@ -7,19 +7,19 @@
     tabstop = 2;
     laststatus = 3;
     completeopt = "menu,menuone,noselect";
-    conceallevel = 2; # Hide * markup for bold and italic, but not markers with substitutions
-    confirm = true; # Confirm to save changes before exiting modified buffer
-    cursorline = true; # Enable highlighting of the current line
-    expandtab = true; # Use spaces instead of tabs
-    formatoptions = "jcroqlnt"; # tcqj
+    conceallevel = 2;
+    confirm = true;
+    cursorline = true;
+    expandtab = true;
+    formatoptions = "jcroqlnt";
     grepformat = "%f:%l:%c:%m";
-    ignorecase = true; # Ignore case
-    inccommand = "nosplit"; # preview incremental substitute
-    list = true; # Show some invisible characters (tabs...
-    mouse = "a"; # Enable mouse mode
-    pumblend = 10; # Popup blend
-    pumheight = 10; # Maximum number of entries in a popup
-    scrolloff = 4; # Lines of context
+    ignorecase = true;
+    inccommand = "nosplit";
+    list = true;
+    mouse = "a";
+    pumblend = 10;
+    pumheight = 10;
+    scrolloff = 4;
     sessionoptions = [
       "buffers"
       "curdir"
@@ -30,21 +30,21 @@
       "skiprtp"
       "folds"
     ];
-    shiftround = true; # Round indent
-    showmode = false; # Dont show mode since we have a statusline
-    sidescrolloff = 8; # Columns of context
-    signcolumn = "yes"; # Always show the signcolumn, otherwise it would shift the text each time
-    smartcase = true; # Don't ignore case with capitals
-    smartindent = true; # Insert indents automatically
-    splitbelow = true; # Put new windows below current
+    shiftround = true;
+    showmode = false;
+    sidescrolloff = 8;
+    signcolumn = "yes";
+    smartcase = true;
+    smartindent = true;
+    splitbelow = true;
     splitkeep = "screen";
-    splitright = true; # Put new windows right of current
-    termguicolors = true; # True color support
-    spelllang = lib.mkDefault [ "en_us" ]; # Spell check languages
+    splitright = true;
+    termguicolors = true;
+    spelllang = lib.mkDefault [ "en_us" ];
     showmatch = true;
     infercase = true;
     clipboard = "unnamedplus";
-    foldlevel = 99; # Folds with a level higher than this number will be closed
+    foldlevel = 99;
     foldcolumn = "0";
     foldenable = true;
     foldlevelstart = 99;
